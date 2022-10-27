d0091baa8817943170c09611813a6ad5bc0645d0
PBBond <97637272+PBBond@users.noreply.github.com>
2022-10-27 17:13:19 -0500

    figured out pagination

--- src/app/differential/numpad/print-dialog/print-dialog.component.ts
+++ src/app/differential/numpad/print-dialog/print-dialog.component.ts
@@ -40,7 +40,7 @@ export class PrintDialogComponent {
   pageRows: Row[][] = [[...this.presetService.currentPreset.rows]];
   constructor(private presetService: PresetService) {
     setTimeout(() => {
-      this.work();
+      this.refactor();
     });
   }
 
@@ -59,7 +59,7 @@ export class PrintDialogComponent {
     //timeout is needed because it needs to wait for the page to rerender
     //before calculating the div height
     setTimeout(() => {
-      this.work();
+      this.refactor();
     });
   }
 
@@ -93,46 +93,112 @@ export class PrintDialogComponent {
     window.print();
   }
 
-  getNumPages() {
-    let element = this.previewPage.nativeElement;
-    return Math.ceil(element.scrollHeight / element.clientHeight);
+  getTotal(rowSpace = false) {
+    let title = 1;
+    let fields = Math.ceil(this.fields.length / 2);
+    let count = this.allSettings.showWBC ? 2 : 1;
+    let tableHeader = this.allSettings.showLabels ? 1 : 0;
+    let ignoredRows = this.allSettings.showIgnored ? this.getNumIgnored() : 0;
+    let normalRows =
+      this.presetService.currentPreset.rows.length - this.getNumIgnored();
+
+    let total = title + fields + count + tableHeader + ignoredRows + normalRows;
+    let rows = 20 - (title + fields + count + tableHeader);
+
+    return rowSpace ? rows : total;
   }
 
-  isFull() {
-    let numPages = this.pageRows.length;
-
-    let element = this.previewPage.nativeElement;
-    let pane = this.previewPane.nativeElement;
-    let ch = element.clientHeight;
-    let sh = element.scrollHeight;
-    console.log(ch);
-    console.log(sh);
-    console.log(pane.scrollHeight);
-
-    let result = sh > (ch + 5) * numPages;
-    //console.log(result);
-    //console.log((ch + 5) * numPages);
-
-    return sh > ch + 5;
-  }
-
-  async work() {
-    while (this.isFull()) {
-      let removed = this.pageRows[0].pop();
-      if (!this.pageRows[1]) {
-        this.pageRows[1] = [removed!];
-      } else {
-        this.pageRows[1] = [removed!, ...this.pageRows[1]];
+  getNumIgnored() {
+    let count = 0;
+    for (let item of this.presetService.currentPreset.rows) {
+      if (item.ignore) {
+        count++;
       }
-      await this.sleep(1);
     }
-    let numPages = this.pageRows.length;
-    this.pages = Array(numPages - 1)
-      .fill(0)
-      .map((x, i) => i);
+    return count;
   }
 
-  sleep(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+  refactor() {
+    let total = this.getTotal();
+    //if we need to add extra pages
+    if (total > 20) {
+      //let NumNewPages = Math.ceil((total - 20) / 20);
+      let page1RowSpaces = this.getTotal(true);
+      let rows: Row[][] = [];
+      let pageIndex = 0;
+      for (let row of this.presetService.currentPreset.rows) {
+        if (this.allSettings.showIgnored) {
+          rows[pageIndex] = rows[pageIndex] ? [...rows[pageIndex], row] : [row];
+        }
+        //if not displaying ignored rows only push non-ignored rows
+        else {
+          if (!row.ignore) {
+            rows[pageIndex] = rows[pageIndex]
+              ? [...rows[pageIndex], row]
+              : [row];
+          }
+        }
+        //if it's the first page, we only have page1rowspaces amount of space
+        if (pageIndex === 0) {
+          if (rows[0].length === page1RowSpaces) {
+            pageIndex++;
+          }
+        }
+        //all other pages have 20 space
+        else {
+          if (rows[pageIndex].length === 20) {
+            pageIndex++;
+          }
+        }
+      }
+
+      this.pageRows = [...rows];
+      this.pages = Array(this.pageRows.length - 1)
+        .fill(0)
+        .map((x, i) => i);
+    }
   }
+
+  // getNumPages() {
+  //   let element = this.previewPage.nativeElement;
+  //   return Math.ceil(element.scrollHeight / element.clientHeight);
+  // }
+
+  // isFull() {
+  //   let numPages = this.pageRows.length;
+
+  //   let element = this.previewPage.nativeElement;
+  //   let pane = this.previewPane.nativeElement;
+  //   let ch = element.clientHeight;
+  //   let sh = element.scrollHeight;
+  //   console.log(ch);
+  //   console.log(sh);
+  //   console.log(pane.scrollHeight);
+
+  //   let result = sh > (ch + 5) * numPages;
+  //   //console.log(result);
+  //   //console.log((ch + 5) * numPages);
+
+  //   return sh > ch + 5;
+  // }
+
+  // async work() {
+  //   while (this.isFull()) {
+  //     let removed = this.pageRows[0].pop();
+  //     if (!this.pageRows[1]) {
+  //       this.pageRows[1] = [removed!];
+  //     } else {
+  //       this.pageRows[1] = [removed!, ...this.pageRows[1]];
+  //     }
+  //     await this.sleep(1);
+  //   }
+  //   let numPages = this.pageRows.length;
+  //   this.pages = Array(numPages - 1)
+  //     .fill(0)
+  //     .map((x, i) => i);
+  // }
+
+  // sleep(ms: number) {
+  //   return new Promise((resolve) => setTimeout(resolve, ms));
+  // }
 }
