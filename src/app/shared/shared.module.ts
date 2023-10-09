747ceb35f84fa44c63e73196235511eaa5dac3e8
pavlikbond <64117050+pavlikbond@users.noreply.github.com>
2023-10-09 11:48:11 -0500

    small checkout button changes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CheckoutButtonComponent } from './components/checkout-button/checkout-button.component';
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
-  declarations: [
-    CheckoutButtonComponent
-  ],
-  exports: [],
+  declarations: [CheckoutButtonComponent],
+  imports: [MatProgressSpinnerModule, MatButtonModule],
+  exports: [CheckoutButtonComponent],
 })
 export class SharedModule {}
